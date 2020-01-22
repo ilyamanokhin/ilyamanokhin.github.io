3074f8f67d77b6b14d1ac6f63a388234e97ba435
Ilya <manokhinia@mosreg.ru>
2020-01-22 13:09:20 +0300

    Исправил gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,10 @@ const isSync = (process.argv.indexOf('--sync') !== -1);
 
 
 gulp.task('default', function () {
-	return gulp.src('*.*', {read: false})
-		.pipe(gulp.dest('./src/img'))
-		.pipe(gulp.dest('./src/img/content'))
-		.pipe(gulp.dest('./src/img/icons'))
+	return gulp.src('*.*', { read: false })
+		.pipe(gulp.dest('./src/assets'))
+		.pipe(gulp.dest('./src/assets/img'))
+		.pipe(gulp.dest('./src/assets/icons'))
 		.pipe(gulp.dest('./src/fonts'))
 		.pipe(gulp.dest('./src/less'))
 		.pipe(gulp.dest('./src/less/blocks'))
@@ -45,93 +45,95 @@ let cssFiles = [
 ];
 */
 const jsFiles = [
-	'./src/js/debugGridCreate.js',
-	 './src/js/main.js',
+	'./src/js/debug-grid-create.js',
+	'./src/js/main.js',
 ];//порядок сборки js файлов
 
-function clear(){
+function clear() {
 	return del('build/*');
 }
-
-function styles(){
-	return gulp.src('./src/less/+(styles|styles-per).less')
-			   .pipe(gulpif(isDev, sourcemaps.init()))
-			   .pipe(less())
-			   //.pipe(concat('style.css')) 
-			   //.pipe(concat('all.min.css'))
-			   .pipe(gcmq())
-			   .pipe(autoprefixer({
-				   //in package.json
-		        }))
-			   //.on('error', console.error.bind(console))
-			   .pipe(gulpif(isProd, cleanCSS({
-			   		level: 2
-			   })))
-			   .pipe(gulpif(isDev, sourcemaps.write()))
-			   .pipe(gulp.dest('./build/css'))
-			   .pipe(gulpif(isSync, browserSync.stream()));
-}
-function scripts () {
-	 return gulp.src(jsFiles)
-				.pipe(gulpif(isDev, sourcemaps.init()))
-				// .pipe(concat('all.js')) 
-				// .pipe(concat('all.min.js'))
-				// .pipe(uglify({
-				// 	toplevel: false//жесткое сжатие js
-				// }))
-				.pipe(gulpif(isDev, sourcemaps.write()))
-				.pipe(gulp.dest('./build/js'))
-				.pipe(gulpif(isSync, browserSync.stream()));
-		
-
+function sprite() {
+	return gulp.src('./src/assets/icons/*.png').pipe(spritesmith({
+		imgName: 'sprite.png',
+		imgPath: '../assets/icons/sprite.png',
+		padding: 10,
+		cssName: 'sprite.less',
+	}))
+		.pipe(gulp.dest('./build/assets/icons/'))
+		.pipe(gulp.dest('./src/less/sprite'))
 }
 
-function fonts(){
+function fonts() {
 	return gulp.src('./src/fonts/**/*')
-			   .pipe(gulp.dest('./build/fonts'))
+		.pipe(gulp.dest('./build/fonts'))
 }
 
 function images() {
-	return gulp.src(['./src/img/*', '!./src/img/icons'])
-		.pipe(imagemin({ optimizationLevel: 3,
-			svgoPlugins: [{removeViewBox: false}],
+	return gulp.src(['./src/assets/img/*', '!./src/assets/icons'])
+		.pipe(imagemin({
+			optimizationLevel: 3,
+			svgoPlugins: [{ removeViewBox: false }],
 			use: [pngquant()],
 			progressive: true,
-			interlaced: true }))
-		.pipe(gulp.dest('./build/img'))
-		// .pipe(notify({ message: 'Images task complete' }));
-}
-function sprite() {
-	return gulp.src('./src/img/icons/*.png').pipe(spritesmith({
-		imgName: 'sprite.png',
-		imgPath: './build/img/sprite.png',
-		padding:10,
-		cssName: 'sprite.less',
-	  }))
-	  .pipe(gulp.dest('./build/img/'))
-	  .pipe(gulp.dest('./src/less/sprite'))
+			interlaced: true
+		}))
+		.pipe(gulp.dest('./build/assets/img/'))
+	// .pipe(notify({ message: 'Images task complete' }));
 }
 
-function html(){
+function html() {
 	return gulp.src('./src/*.html')
-			   .pipe(gulp.dest('./build'))
-			   .pipe(gulpif(isSync, browserSync.stream()));
-}
-function pugc(){
-	return gulp.src(['./src/**/*.pug','!./src/modules/*.pug','!./src/templates/*.pug'])
-			.pipe(pug({pretty: '\t'}))
-			.pipe(gulp.dest('./build'))
-			.pipe(gulpif(isSync, browserSync.stream()));
+		.pipe(gulp.dest('./build'))
+		.pipe(gulpif(isSync, browserSync.stream()));
 }
 
-function watch(){
-	if(isSync){
+function pugc() {
+	return gulp.src(['./src/**/*.pug','!./src/**/content.pug', '!./src/modules/*.pug', '!./src/blocks/*.pug', '!./src/blocks/**/*.pug', '!./src/templates/*.pug'])
+		.pipe(pug({ pretty: '\t' }))
+		.pipe(gulp.dest('./build'))
+		.pipe(gulpif(isSync, browserSync.stream()));
+}
+
+function styles() {
+	return gulp.src('./src/less/+(styles|styles-per).less')
+		.pipe(gulpif(isDev, sourcemaps.init()))
+		.pipe(less())
+		//.pipe(concat('style.css')) 
+		//.pipe(concat('all.min.css'))
+		.pipe(gcmq())
+		.pipe(autoprefixer({
+			//in package.json
+		}))
+		//.on('error', console.error.bind(console))
+		.pipe(gulpif(isProd, cleanCSS({
+			level: 2
+		})))
+		.pipe(gulpif(isDev, sourcemaps.write()))
+		.pipe(gulp.dest('./build/css'))
+		.pipe(gulpif(isSync, browserSync.stream()));
+}
+
+function scripts() {
+	return gulp.src(jsFiles)
+		.pipe(gulpif(isDev, sourcemaps.init()))
+		// .pipe(concat('all.js')) 
+		// .pipe(concat('all.min.js'))
+		// .pipe(uglify({
+		// 	toplevel: false//жесткое сжатие js
+		// }))
+		.pipe(gulpif(isDev, sourcemaps.write()))
+		.pipe(gulp.dest('./build/js'))
+		.pipe(gulpif(isSync, browserSync.stream()));
+}
+
+function watch() {
+	if (isSync) {
 		browserSync.init({
-	        server: {
-	            baseDir: "./build/",
+			server: {
+				baseDir: "./build/",
 			}
 			// tunnel: true //использование локального сервера
-	    });
+		});
 	}
 
 	gulp.watch('./src/less/**/*.less', styles);
@@ -141,7 +143,7 @@ function watch(){
 	gulp.watch('./smartgrid.js', grid);
 }
 
-function grid(done){
+function grid(done) {
 	delete require.cache[require.resolve('./smartgrid.js')];
 
 	let settings = require('./smartgrid.js');
@@ -154,12 +156,11 @@ function grid(done){
 	done();
 }
 
-
-
-let build = gulp.series(clear, 
-	gulp.parallel(styles, scripts, sprite, images, fonts, html, pugc)
+let build = gulp.series(clear,
+	gulp.parallel(sprite, images, fonts, html, pugc, styles, scripts)
 );
 
 gulp.task('build', gulp.series(grid, build));
 gulp.task('watch', gulp.series(build, watch));
 gulp.task('grid', grid);
+gulp.task('sprite', sprite);
