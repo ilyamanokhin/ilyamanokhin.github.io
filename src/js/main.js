5e8d294a15880593b24a54c877d1e2b65d1ac27e
Ilya <manokhinia@mosreg.ru>
2020-03-31 11:50:24 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
-let btnCreate = document.querySelector('.js__create-modal');
-let modal = document.querySelector('.modal');
-let btnClose = document.querySelector('.page__btn_close');
+const btnCreate = document.querySelector('.js__create-modal');
+const modal = document.querySelector('.modal');
+const btnClose = document.querySelector('.page__btn_close');
+const body = document.querySelector('body');
+
+
 btnCreate.addEventListener("click", ()=>{
-    modal.classList.remove('page_hidden')
+    modal.classList.remove('page_hidden');
+    body.classList.toggle('page_overflow-hidden');
 });
 btnClose.addEventListener("click", ()=>{
-    modal.classList.add('page_hidden')
+    modal.classList.add('page_hidden');
+    body.classList.toggle('page_overflow-hidden');
 });
