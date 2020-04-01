3999a9cc5b9b80790404572d4ff63599ddb21c24
Ilya <manokhinia@mosreg.ru>
2020-04-01 13:50:28 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,42 @@
-const btnCreate = document.querySelector('.js__create-modal');
-const modal = document.querySelector('.modal');
-const btnClose = document.querySelector('.page__btn_close');
 const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const btnCreate = document.querySelector('.js__create-modal');
+const btnClose = document.querySelector('.page__btn_close');
 
 
-btnCreate.addEventListener("click", ()=>{
-    modal.classList.remove('page_hidden');
+function toggleModal() {
+    modal.classList.toggle('page_hidden');
     body.classList.toggle('page_overflow-hidden');
-});
-btnClose.addEventListener("click", ()=>{
-    modal.classList.add('page_hidden');
-    body.classList.toggle('page_overflow-hidden');
-});
+}
+if (btnCreate) {
+    btnCreate.addEventListener("click", () => {
+        toggleModal();
+    });
+}
+if (btnClose) {
+    btnClose.addEventListener("click", () => {
+        toggleModal();
+    });
+}
+
+const debounce = (fn) => {
+    let frame;
+    return (...params) => {
+
+        if (frame) {
+            cancelAnimationFrame(frame);
+        }
+
+        frame = requestAnimationFrame(() => {
+            fn(...params);
+        });
+    }
+};
+
+const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+}
+
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+storeScroll();
