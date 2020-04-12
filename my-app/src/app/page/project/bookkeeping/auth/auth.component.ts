25415bac0358d6cf3394faf73731688ecbce7654
Ilya <manokhinia@mosreg.ru>
2020-04-12 16:40:16 +0300

    del

--- my-app/src/app/page/project/bookkeeping/auth/auth.component.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule, Component } from '@angular/core';
-@Component({
-    selector: 'app-auth',
-    templateUrl: './auth.component.html',
-    styleUrls: ['./auth.component.less']
-
-})
-
-export class AuthComponent {}
